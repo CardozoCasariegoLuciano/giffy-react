63bb07aca887c450886bfc47b94dc439f2c51b21
Luciano Cardozo Casariego <lcardozo@mobydigital.com>
2022-07-14 23:12:31 -0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route } from "wouter";
+import {Router, Route } from "wouter";
 import Home from "./pages/Home/Home";
 import GifDetails from "./pages/GifDetails/GifDetails";
 import SearchGifs from "./components/SearchGifs/SearchGifs";
@@ -9,10 +9,12 @@ function App() {
   return (
     <GifContextProvider>
       <div className="App">
-        <Route path="/" component={Home} />
-        <Route path="/search/:keyword/:rating?" component={SearchGifs} />
-        <Route path="/gif/:id" component={GifDetails} />
-        <Route path="/404" component={() => <h1>Erorr 404</h1>} />
+ 				<Router base="/giffy">
+        	<Route path="/" component={Home} />
+        	<Route path="/search/:keyword/:rating?" component={SearchGifs} />
+       		<Route path="/gif/:id" component={GifDetails} />
+        	<Route path="/404" component={() => <h1>Erorr 404</h1>} />
+ 				</Router>
       </div>
     </GifContextProvider>
   );
